2f4b9338bd8d83d4d7574b0282c448a5ebdd2aad
Cotze <carlosrodrigoatl@gmail.com>
2022-02-16 01:09:24 -0600

    Declarados los webservice pero se cambio la base de datos

---
+++ Proyecto3Capas/BLL/BLLEnvios.cs
@@ -0,0 +1,20 @@
+﻿using Proyecto3Capas.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Proyecto3Capas.BLL
+{
+    public class BLLEnvios
+    {
+        //Insertar
+        public static long InsEnvio(int IdCliente, int Idvendedor, int IdOrigen, int IdDestino, double Distancia, DateTime FSalida, DateTime FLlegadaE)
+        {
+            DALClientes.UpdCliente(IdCliente, null, null, null, null, null);
+            DALVendedores.UpdVendedor(Idvendedor, null, null, null, null, null, null);
+            return DALEnvios.InsRuta(IdCliente, Idvendedor, IdOrigen, IdDestino, Distancia, FSalida, FLlegadaE);
+
+        }
+    }
+}
---
+++ Proyecto3Capas/Catalogos/Envios/AltaEnvios.aspx.designer.cs
@@ -0,0 +1,17 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace Proyecto3Capas.Catalogos.Envios
+{
+
+
+    public partial class AltaEnvios
+    {
+    }
+}
